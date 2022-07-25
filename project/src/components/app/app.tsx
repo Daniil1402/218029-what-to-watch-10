f6136914d4210ef563f5fa98ae776df149c9f7b2
Daniil1402 <54863974+Daniil1402@users.noreply.github.com>
2022-07-25 23:42:49 +0300

    module2-task1

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,14 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Main from '../../pages/main/main';
+
+type PromoFilm = {
+  title: string;
+  genre: string;
+  date: number;
+};
+
+function App({title, genre, date}: PromoFilm) {
+
+  return <Main title={title} genre={genre} date={date} />;
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const settings = {
+  title: 'The Grand Budapest Hotel',
+  genre: 'Drama',
+  date: 2014,
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App title={settings.title} genre={settings.genre} date={settings.date}/>
+  </React.StrictMode>
 );
