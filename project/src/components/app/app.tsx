a023b3b301abeea90e59bcb07e6be626455c2cd6
Daniil1402 <54863974+Daniil1402@users.noreply.github.com>
2022-08-02 19:36:20 +0300

    module3-task1

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,13 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import Player from '../../pages/player/player';
+import AddReview from '../../pages/add-review/add-review';
+import E404 from '../../pages/E404/E404';
+import Film from '../../pages/film/film';
 import Main from '../../pages/main/main';
+import MyList from '../../pages/my-list/my-list';
+import SignIn from '../../pages/sign-in/sign-in';
+import PrivateRoute from '../private-route/private-route';
 
 type PromoFilm = {
   title: string;
@@ -6,9 +15,47 @@ type PromoFilm = {
   date: number;
 };
 
-function App({title, genre, date}: PromoFilm) {
-
-  return <Main title={title} genre={genre} date={date} />;
+function App({ title, genre, date }: PromoFilm) {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<Main title={title} genre={genre} date={date} />}
+        />
+        <Route
+          path={AppRoute.SignIn}
+          element={<SignIn />}
+        />
+        <Route
+          path={AppRoute.MyList}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <MyList />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Film}
+          element={<Film />}
+        />
+        <Route
+          path={AppRoute.AddReview}
+          element={<AddReview />}
+        />
+        <Route
+          path={AppRoute.Player}
+          element={<Player/>}
+        />
+        <Route
+          path="*"
+          element={<E404 />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
